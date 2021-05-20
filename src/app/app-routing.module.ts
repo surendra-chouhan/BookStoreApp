03b8bda1d2722801f2c9916303e53ed9960fe2d1
surendra-chouhan <chouhansurendra028@gmail.com>
2021-05-20 19:43:53 +0530

    Added Routes in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './component/adminLogin/admin-login/admin-login.component';
+import { HeaderComponent } from './component/header/header/header.component';
+import { FooterComponent } from './component/footer/footer/footer.component';
+import { AdminDashboardComponent } from './component/adminDashboard/admin-dashboard/admin-dashboard.component'
 
 const routes: Routes = [
-  // {
-  //   path: "",
-  //   redirectTo: "login",
-  //   pathMatch: "full"
-  // },
   {
     path: 'login',
     component: AdminLoginComponent
+  },
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: 'footer',
+    component: FooterComponent
+  },
+  {
+    path: 'dashboard',
+    component: AdminDashboardComponent
   }
-  // {
-  //   path: "**",
-  //   redirectTo: "reset-password",
-  //   pathMatch: "full"
-  // }
+  
 ];
 
 @NgModule({
