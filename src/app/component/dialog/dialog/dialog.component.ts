8c0ab87b8031ac0c44b5f42dffafe2b707aafb9c
surendra-chouhan <chouhansurendra028@gmail.com>
2021-05-25 20:21:12 +0530

    Added addBook API

--- src/app/component/dialog/dialog/dialog.component.ts
+++ src/app/component/dialog/dialog/dialog.component.ts
@@ -1,14 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminserviceService } from 'src/app/Services/adminservice/adminservice.service';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
+
 export class DialogComponent implements OnInit {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder, private adminService: AdminserviceService) {
 
-  constructor() { }
+    this.form = this.formBuilder.group({
 
+      bookName: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      quantity: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      discount: ['', [Validators.required]]
+    });
+  }
+
+  addBook() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+    let reqObj = {
+      bookName: this.form.value.bookName,
+      author: this.form.value.author,
+      description: this.form.value.description,
+      quantity: this.form.value.quantity,
+      price: this.form.value.price,
+      discountPrice: this.form.value.discount
+    }
+
+    console.log(reqObj)
+    this.adminService.addBook(reqObj).subscribe((res) => {
+      console.log(res);
+    }, (error) => {
+      console.log(error);
+    })
+  }
   ngOnInit(): void {
   }
 
